2a5694f0b50d9650ba9e69e7ab17ddbfb9f8cad3
BuildTools <bilaltehseen@gmail.com>
2020-08-30 20:10:52 +0500

    Lab3 and Multisystemmodel changed

--- Lab3.m
+++ Lab3.m
@@ -2,22 +2,30 @@ clear all;
 close all;
 clc;
 X0=[0;0;0;0];	% (Initial xb, Vb, xa, Va)
-[t,X]=ode45('multiple_element_sys',[0 200],X0);
 figure;
-subplot(2,1,1);
-plot(t,X(:,1));
-plot(t,X(:,2),'r');
+for change=0:50:150   
+    [t,X]=ode45(@(t,y)multiple_element_sys(t,y,change,100,100,100,100,100,100,100),[0 200],X0);
+    subplot(2,1,1);
+    plot(t,X(:,1),'DisplayName',[num2str(change),' % Mass Xb' ]);
+    hold on
+    plot(t,X(:,2),'DisplayName',[num2str(change),' % Mass Vb' ]);
+end
+legend('show');
+hold on
 xlabel('Time(t)');
 ylabel('Position xb / Speed Vb');
 title('Mass spring system');
-legend('xb', 'Vb');
 grid;
-subplot(2,1,2);
-plot(t,X(:,3));
-hold;
-plot(t,X(:,4),'r');
+for change=0:50:150   
+    [t,X]=ode45(@(t,y)multiple_element_sys(t,y,change,100,100,100,100,100,100,100),[0 200],X0);
+    subplot(2,1,2);
+    plot(t,X(:,3),'DisplayName',[num2str(change),' % Mass Xa' ]);
+    hold on
+    plot(t,X(:,4),'DisplayName',[num2str(change),' % Mass Va' ]);
+end
+legend('show');
+hold on
 xlabel('Time(t)');
 ylabel('Position xa / Speed Va');
 title('Mass spring system');
-legend('xa', 'Va');
 grid;
